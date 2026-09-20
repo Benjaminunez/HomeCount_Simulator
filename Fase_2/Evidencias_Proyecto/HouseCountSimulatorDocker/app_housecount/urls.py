7f85c904c6550695fd3325c46f73ec78dcfa2eba
Benjamin <benj.nuneza@duocuc.cl>
2026-09-20 18:10:10 -0300

    descripción ruta main del proyecto

--- Fase_2/Evidencias_Proyecto/HouseCountSimulatorDocker/app_housecount/urls.py
+++ Fase_2/Evidencias_Proyecto/HouseCountSimulatorDocker/app_housecount/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'), #vista inicial de la pagina al iniciar con localhost (este queda como main)
     path('inicio.html', views.inicio, name='inicio_html'),
     path('contrucion.html', views.contrucion, name='contrucion'),
     path('casas_rapidas.html', views.casas_rapidas, name='casas_rapidas'),
