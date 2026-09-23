121ae29d5b8f8dc359acd66943e178315dc4ef4a
Benjamin <benj.nuneza@duocuc.cl>
2026-09-22 22:18:39 -0300

    prueba de api creada mediante Gist Github

--- Fase_2/Evidencias_Proyecto/HouseCountSimulatorDocker/app_housecount/views.py
+++ Fase_2/Evidencias_Proyecto/HouseCountSimulatorDocker/app_housecount/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import ModeloCasa
+import requests
 
 def inicio(request): return render(request, 'inicio.html')
 def contrucion(request): return render(request, 'contrucion.html')
@@ -26,14 +27,31 @@ def casas_rapidas(request):
         modelos = modelos.filter(habitaciones__gte=habitaciones)
     if banos:
         modelos = modelos.filter(banos__gte=banos)
+
+    casas_mercado = []
+    
+    try:
+        
+        url_api = "https://gist.githubusercontent.com/Benjaminunez/b367b26daa23044dd9ee7ee14ad3a8e0/raw/ac4792063436f31d0218f2025dccac3a2a285753/casas_api.json" 
+        
+        # Una petición limpia, sin headers ni bloqueos
+        respuesta = requests.get(url_api, timeout=5)
+        
+        if respuesta.status_code == 200:
+            casas_mercado = respuesta.json() # Los datos ya vienen listos
+            
+    except Exception as e:
+        print(f"Error al conectar con la API: {e}")
     
     context = {
-        'modelos': modelos,
-        'filtros': {
-            'precio_max': precio_max or '',
-            'm2_min': m2_min or '',
-            'habitaciones': habitaciones or '',
-            'banos': banos or '',
+            'modelos': modelos,
+            'filtros': {
+                'precio_max': precio_max or '',
+                'm2_min': m2_min or '',
+                'habitaciones': habitaciones or '',
+                'banos': banos or '',
+            },
+            'casas_mercado': casas_mercado,
         }
-    }
+        
     return render(request, 'casas_rapidas.html', context)
